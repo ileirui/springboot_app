6ff0c9f2ce5f615c32abea60f453b71d1247e42e
wjd <66666@666.com>
2020-09-28 17:35:26 +0800

    change

--- src/main/java/com/qa/springboot/A.java
+++ src/main/java/com/qa/springboot/A.java
@@ -2,6 +2,19 @@ package com.qa.springboot;
 
 public class A {
 
-	public String x2;
-	public Integer xl2;
+	private String x2;
+	private Integer xl2;
+	public String getX2() {
+		return x2;
+	}
+	public void setX2(String x2) {
+		this.x2 = x2;
+	}
+	public Integer getXl2() {
+		return xl2;
+	}
+	public void setXl2(Integer xl2) {
+		this.xl2 = xl2;
+	}
+	
 }
--- src/main/java/com/qa/springboot/B.java
+++ src/main/java/com/qa/springboot/B.java
@@ -4,4 +4,17 @@ public class B {
 
 	private String gly;
 	private Integer gInteger;
+	public String getGly() {
+		return gly;
+	}
+	public void setGly(String gly) {
+		this.gly = gly;
+	}
+	public Integer getgInteger() {
+		return gInteger;
+	}
+	public void setgInteger(Integer gInteger) {
+		this.gInteger = gInteger;
+	}
+	
 }
